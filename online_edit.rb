9bbbdf3ebf0b157d6ba428b942056fcaf05c99ac
Vivek T <Vivek.T@prevas.com>
2014-01-20 13:35:05 +0530

    added bat file

--- online_edit.rb
+++ online_edit.rb
@@ -5,6 +5,10 @@
 
 require 'rubygems'
 require 'open-uri'
+#!/usr/bin/env ruby
+#ARGV.each do|a|
+  #puts "Argument: #{a}"
+#end
 
 class OnlineEdit
 
@@ -67,6 +71,9 @@ class OnlineEdit
 end
 
 # this is just a test run, 3 parameters will be passed to the file - path,file&repository name
-init = OnlineEdit.new('https://tstpd.pdprojects.prevas.com/svn/aef012.documents','README.txt','aef012.documents')
-init.create_tmp_folder
+#init = OnlineEdit.new('https://tstpd.pdprojects.prevas.com/svn/aef012.documents','README.txt','aef012.documents')
+#init.create_tmp_folder
+#podiumonlineedit:?url=https://tstpd.pdprojects.prevas.com/svn/aef012.documents=README.txt=aef012.documents
 
+init = OnlineEdit.new(ARGV[1].to_s,ARGV[2].to_s,ARGV[3].to_s)
+init.create_tmp_folder
